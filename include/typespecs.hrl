750838d460982ec821f7717c0390033c18b4a639
Yurii Rashkovskii <yrashk@spawngrid.com>
2011-01-23 02:02:15 -0800

    Reindented typespecs.hrl

--- include/typespecs.hrl
+++ include/typespecs.hrl
@@ -11,22 +11,22 @@
 		{stop, any()}.
 
 -type gen_server_handle_call_result(State) ::
-						 {reply, any(), State} |
-						 {reply, any(), State, timeout()} |
-						 {noreply, State} |
-						 {noreply, State, timeout()} |
-						 {stop, any(), any(), State} |
-						 {stop, any(), State}.
+		{reply, any(), State} |
+		{reply, any(), State, timeout()} |
+		{noreply, State} |
+		{noreply, State, timeout()} |
+		{stop, any(), any(), State} |
+		{stop, any(), State}.
 
 -type gen_server_handle_cast_result(State) ::
-						 {noreply, State} |
-						 {noreply, State, timeout()} |
-						 {stop, any(), State}.
+		{noreply, State} |
+		{noreply, State, timeout()} |
+		{stop, any(), State}.
 
 -type gen_server_handle_info_result(State) ::
-						 {noreply, State} |
-						 {noreply, State, timeout()} |
-						 {stop, any(), State}.
+		{noreply, State} |
+		{noreply, State, timeout()} |
+		{stop, any(), State}.
 
 -type gen_server_terminate_result() :: any().
 
