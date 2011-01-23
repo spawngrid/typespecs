0e096d9855aaccab538a3f40f96a67b6b0b30a72
Yurii Rashkovskii <yrashk@spawngrid.com>
2011-01-23 03:29:30 -0800

    Refactored some standard types (gen_server)

--- include/typespecs.hrl
+++ include/typespecs.hrl
@@ -12,29 +12,39 @@
 
 -type gen_server_init_result() :: gen_server_init_result(any()).
 
--type gen_server_handle_call_result(Reply, Reason) ::
-		{reply, Reply, gen_server_state()} |
-		{reply, Reply, gen_server_state(), timeout()} |
+-type gen_server_no_reply() ::
 		{noreply, gen_server_state()} |
-		{noreply, gen_server_state(), timeout()} |
-		{stop, Reason, Reply, gen_server_state()} |
+		{noreply, gen_server_state(), timeout()}.
+
+-type gen_server_stop(Reason) ::
 		{stop, Reason, gen_server_state()}.
+
+-type gen_server_stop(Reply, Reason) ::
+			{stop, Reason, Reply, gen_server_state()} |
+			gen_server_stop(Reason).
+
+-type gen_server_reply(Reply) ::
+		{reply, Reply, gen_server_state()} |
+		{reply, Reply, gen_server_state(), timeout()}.
+
+-type gen_server_handle_call_result(Reply, Reason) ::
+		gen_server_reply(Reply) |
+		gen_server_no_reply() |
+		gen_server_stop(Reply, Reason).
 
 -type gen_server_handle_call_result(Reply) :: gen_server_handle_call_result(Reply, any()).
 
 -type gen_server_handle_call_result() :: gen_server_handle_call_result(any(), any()).
 
 -type gen_server_handle_cast_result(Reason) ::
-		{noreply, gen_server_state()} |
-		{noreply, gen_server_state(), timeout()} |
-		{stop, Reason, gen_server_state()}.
+		gen_server_no_reply() |
+		gen_server_stop(Reason).
 
 -type gen_server_handle_cast_result() :: gen_server_handle_cast_result(any()).
 
 -type gen_server_handle_info_result(Reason) ::
-		{noreply, gen_server_state()} |
-		{noreply, gen_server_state(), timeout()} |
-		{stop, Reason, gen_server_state()}.
+		gen_server_no_reply() |
+		gen_server_stop(Reason).
 
 -type gen_server_handle_info_result() :: gen_server_handle_info_result(any()).
 
