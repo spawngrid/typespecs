7bb476e664a1cc281c782e6568d23c08e9ea742e
Yurii Rashkovskii <yrashk@spawngrid.com>
2011-01-23 03:03:53 -0800

    Added a capability to specify Result/Reason types

--- include/typespecs.hrl
+++ include/typespecs.hrl
@@ -3,30 +3,40 @@
 
 -type gen_server_from() :: {pid(), gen_server_unique_tag()}.
 
--type gen_server_init_result() ::
+-type gen_server_init_result(Reason) ::
 		{ok, gen_server_state()} |
 		{ok, gen_server_state(), timeout()} |
 		{ok, gen_server_state(), hibernate} |
 		ignore |
-		{stop, any()}.
+		{stop, Reason}.
 
--type gen_server_handle_call_result() ::
-		{reply, any(), gen_server_state()} |
-		{reply, any(), gen_server_state(), timeout()} |
+-type gen_server_init_result() :: gen_server_init_result(any()).
+
+-type gen_server_handle_call_result(Reply, Reason) ::
+		{reply, Reply, gen_server_state()} |
+		{reply, Reply, gen_server_state(), timeout()} |
 		{noreply, gen_server_state()} |
 		{noreply, gen_server_state(), timeout()} |
-		{stop, any(), any(), gen_server_state()} |
-		{stop, any(), gen_server_state()}.
+		{stop, Reason, Reply, gen_server_state()} |
+		{stop, Reason, gen_server_state()}.
 
--type gen_server_handle_cast_result() ::
+-type gen_server_handle_call_result(Reply) :: gen_server_handle_call_result(Reply, any()).
+
+-type gen_server_handle_call_result() :: gen_server_handle_call_result(any(), any()).
+
+-type gen_server_handle_cast_result(Reason) ::
 		{noreply, gen_server_state()} |
 		{noreply, gen_server_state(), timeout()} |
-		{stop, any(), gen_server_state()}.
+		{stop, Reason, gen_server_state()}.
 
--type gen_server_handle_info_result() ::
+-type gen_server_handle_cast_result() :: gen_server_handle_cast_result(any()).
+
+-type gen_server_handle_info_result(Reason) ::
 		{noreply, gen_server_state()} |
 		{noreply, gen_server_state(), timeout()} |
-		{stop, any(), gen_server_state()}.
+		{stop, Reason, gen_server_state()}.
+
+-type gen_server_handle_info_result() :: gen_server_handle_info_result(any()).
 
 -type gen_server_terminate_result() :: any().
 
